e678cf5c1ab82db80e264c8ef6e98e1a6277dced
wuhao930406 <1148547900@qq.com>
2019-07-15 19:34:32 +0800

    index complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-alert(0)
+layui.use(['carousel','layer', 'form','element'], function(){
+    var layer = layui.layer
+    ,form = layui.form;
+    
+    layer.msg('Hello World');
+    var carousel = layui.carousel
+    ,form = layui.form;
+    var element = layui.element;
+    //常规轮播
+    carousel.render({
+      elem: '#test1'
+      ,arrow: 'always'
+    });
+  });
